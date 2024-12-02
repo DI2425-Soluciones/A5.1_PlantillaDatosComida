92d9cac311b717dfc2d1d237d8df60b6ea5e5674
MANUEL MADRIGAL LOSA <m.madrigallosa@edu.gva.es>
2024-12-02 23:06:23 +0100

    Agregar archivos de proyecto.

---
+++ Comida/MainWindowVM.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comida
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        // ListBox de la izquierda: lista de platos.
+        private ObservableCollection<Plato> _listaPlatos;
+        public ObservableCollection<Plato> ListaPlatos
+        {
+            get { return _listaPlatos; }
+            set
+            {
+                _listaPlatos = value;
+                NotifyPropertyChanged("ListaPlatos");
+            }
+        }
+
+        // ListBox de la izquierda: plato seleccionado.
+        private Plato _platoSeleccionado;
+        public Plato PlatoSeleccionado
+        {
+            get { return _platoSeleccionado; }
+            set
+            {
+                _platoSeleccionado = value;
+                NotifyPropertyChanged("PlatoSeleccionado");
+            }
+        }
+
+        // Formulario derecha: ComboBox, tipos de comida.
+        private ObservableCollection<string> _tiposComida;
+        public ObservableCollection<string> TiposComida
+        {
+            get { return _tiposComida; }
+            set
+            {
+                _tiposComida = value;
+                NotifyPropertyChanged("TiposComida");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            // En el proyecto localizamos las fotos de los platos de comida en la carpeta "FotosPlatos".
+            ListaPlatos = DatosServicio.GetSamples(@".\FotosPlatos");
+            
+            PlatoSeleccionado = null;
+
+            TiposComida = DatosServicio.GetTipos();
+        }
+
+        public void QuitarSeleccionado()
+        {
+            PlatoSeleccionado = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Comida/Plato.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comida
+{
+    class Plato : INotifyPropertyChanged
+    {
+        private string _nombre;
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                _nombre = value;
+                NotifyPropertyChanged("Nombre");
+            }
+        }
+
+        private string _imagen;
+        public string Imagen
+        {
+            get => _imagen;
+            set
+            {
+                _imagen = value;
+                NotifyPropertyChanged("Imagen");
+            }
+        }
+        private string _tipo;
+        public string Tipo
+        {
+            get => _tipo;
+            set
+            {
+                _tipo = value;
+                NotifyPropertyChanged("Tipo");
+            }
+        }
+        private bool _gluten;
+        public bool Gluten
+        {
+            get => _gluten;
+            set
+            {
+                _gluten = value;
+                NotifyPropertyChanged("Gluten");
+            }
+        }
+
+        private bool _soja;
+        public bool Soja
+        {
+            get => _soja;
+            set
+            {
+                _soja = value;
+                NotifyPropertyChanged("Soja");
+            }
+        }
+
+        private bool _leche;
+        public bool Leche
+        {
+            get => _leche;
+            set
+            {
+                _leche = value;
+                NotifyPropertyChanged("Leche");
+            }
+        }
+
+        private bool _sulfitos;
+        public bool Sulfitos
+        {
+            get => _sulfitos;
+            set
+            {
+                _sulfitos = value;
+                NotifyPropertyChanged("Sulfitos");
+            }
+        }
+
+        public Plato(string nombre, string imagen, string tipo, bool gluten, bool soja, bool leche, bool sulfitos)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Tipo = tipo;
+            Gluten = gluten;
+            Soja = soja;
+            Leche = leche;
+            Sulfitos = sulfitos;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
